2dcd9e5569fd91d554ac04e5e69f23e8605b50f3
joserchr <106779909+joserchr@users.noreply.github.com>
2023-06-01 17:09:41 -0600

    Creacion de la pagina

---
+++ src/test/java/com/Xperience/Xperience/XperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Xperience.Xperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
